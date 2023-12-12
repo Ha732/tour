c9886c25f78b07213aed2cace54576f137e65dd7
Ha732 <75130957+Ha732@users.noreply.github.com>
2023-12-12 17:20:38 +0500

    Add files via upload

---
+++ src/Loading.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <>
+   
+    <div className="loading">
+      <h1>loading...</h1>
+    </div>
+    </>
+  )
+};
+
+export default Loading;
